beb02c53caa88b32ade8df84bf07b075a6828999
Keith Marshall <code@eleusis.co.uk>
2018-08-10 08:21:54 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PointsService } from './services/points.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'priorites';
+
+  constructor(public pointsService: PointsService) { 
+    pointsService.addCategory("test");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MdesignModule } from './mdesign/mdesign.module';
+import { PsliderComponent } from './pslider/pslider.component';
+import { PointsService } from './services/points.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PsliderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
